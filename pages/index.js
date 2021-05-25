ef94263d5fabfdf906d220c6a05ed6c6d6412853
EmiPeralta <emilianop96@hotmail.com>
2021-05-24 21:22:14 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+        <meta name='description' content='Homepage' />
+      </Head>
+
+      <h1>Hello world</h1>
+    </div>
+  );
+}
